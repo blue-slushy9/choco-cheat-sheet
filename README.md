8a619f1f90bfcf8acbc091e59e3b71b225977e59
blue-slushy9 <107578323+blue-slushy9@users.noreply.github.com>
2025-02-05 18:21:57 -0800

    Cleaned up commits.

---
+++ README.md
@@ -0,0 +1,1 @@
+# choco-cheat-sheet
---
+++ choco_cheat_sheet.ps1
@@ -0,0 +1,22 @@
+# This is a cheat sheet for using Chocolatey package manager on Windows;
+
+# Upgrade all choco packages (including chocolatey?);
+choco upgrade all
+
+# Search for a package;
+choco search <package name or key word>
+
+# Install a package;
+choco install <package name>
+
+# Reinstall a package;
+choco install --force <package name>
+
+# Uninstall a package;
+choco uninstall <package name>
+
+# Upgrade a package;
+choco upgrade <package name>
+
+# View all installed choco packages;
+choco list
